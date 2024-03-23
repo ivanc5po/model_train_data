08baeb08b79d0cd31f6c44d79a0b2cdf67715ca7
ivanpoopoo <59787536+ivanc5po@users.noreply.github.com>
2024-03-23 20:08:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-apt install python3-pip -y && pip install torch hanziconv datasets && cd model_train_data
+apt install python3-pip -y
+pip install torch hanziconv datasets
+cd model_train_data
+python3 model.py
