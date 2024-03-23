6cb18a9c4b24c1c47c1d07ce8a1a24d6a24f45e7
ivanpoopoo <59787536+ivanc5po@users.noreply.github.com>
2024-03-24 00:29:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@ apt install python3-pip -y
 pip install torch hanziconv datasets requests
 cd model_train_data
 python3 model.py
+
+sudo swapoff /swapfile
+sudo rm /swapfile
+sudo fallocate -l 500G /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
