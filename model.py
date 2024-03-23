13351e2e46307f2dc8d0ee1305619ddc53addd8d
ivanpoopoo <59787536+ivanc5po@users.noreply.github.com>
2024-03-24 04:46:43 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import numpy as np
+import os
+import time
+
 save_dir = 'model'
 
 # Data Preparation
@@ -83,7 +86,19 @@ if __name__ == "__main__":
     ip_list = ["208.68.39.112:12345", "143.244.164.42:12345", "208.68.36.142:12345", "178.128.148.143:12345", "157.230.88.11:12345"]
     cluster_resolver = tf.distribute.cluster_resolver.TFConfigClusterResolver(ip_list)
     cluster_resolver.task_type = 'worker'
+
+    # Wait until all nodes are online
+    while True:
+        cluster_spec = cluster_resolver.cluster_spec()
+        if cluster_spec:
+            all_nodes_online = all(len(cluster_spec.task_addresses(role)) > 0 for role in cluster_spec.task_types)
+            if all_nodes_online:
+                break
+        print("Waiting for all nodes to come online...")
+        time.sleep(1)
+
     strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy(
+        cluster_resolver=cluster_resolver,
         communication=tf.distribute.experimental.CollectiveCommunication.AUTO)
 
     train(strategy, questions, answers, char_to_idx, max_length)
