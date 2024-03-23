0ef67cfaf2c6f154fcd1ed2b9ab37b0d767a7ab4
ivanpoopoo <59787536+ivanc5po@users.noreply.github.com>
2024-03-24 04:38:32 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,26 +1,22 @@
 import tensorflow as tf
 import numpy as np
-import socket
-import os
 
-# 数据集，假设有一组问题和对应的回答
+# Data Preparation
 questions = open("questions.txt", "r", encoding="utf-8").readlines()
 answers = open("answers.txt", "r", encoding="utf-8").readlines()
 
-# 构建字符到索引和索引到字符的映射
 chars = sorted(list(set(''.join(questions + answers))))
 char_to_idx = {ch: i for i, ch in enumerate(chars)}
 idx_to_char = {i: ch for i, ch in enumerate(chars)}
 
-# 将文本转换为Tensor，并确保每个样本的长度一致
 def text_to_tensor(text, char_to_idx, max_length):
     tensor = [char_to_idx[ch] for ch in text if ch in char_to_idx]
     tensor += [0] * (max_length - len(tensor)) 
     return np.array(tensor)
 
-# 获取最大长度
 max_length = max(max(len(question), len(answer)) for question, answer in zip(questions, answers))
 
+# Model Definition
 class QALSTM(tf.keras.Model):
     def __init__(self, input_size, hidden_size, output_size, num_heads):
         super(QALSTM, self).__init__()
@@ -36,68 +32,52 @@ class QALSTM(tf.keras.Model):
         lstm_output = self.lstm(attn_output)
         output = self.fc(lstm_output)
         
-        # Ensure output shape matches target shape
-        output = tf.squeeze(output, axis=0)  # Remove the batch dimension
+        output = tf.squeeze(output, axis=0)
         return output
 
+# Training Function
 def train(strategy, questions, answers, char_to_idx, max_length):
-    # 模型参数
-    input_size = len(chars)  # 输入大小为字符集大小
+    input_size = len(chars)
     hidden_size = 128
-    output_size = len(chars)  # 输出大小与输入大小相同
-    num_heads = 8  # 多头注意力的头数
+    output_size = len(chars)
+    num_heads = 8
 
-    # 创建模型和优化器
     with strategy.scope():
         model = QALSTM(input_size, hidden_size, output_size, num_heads)
         optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
-    # 数据集大小
     dataset_size = len(questions)
 
-    # 训练模型
+    @tf.function
+    def train_step(question_tensor, answer_tensor):
+        with tf.GradientTape() as tape:
+            output = model(question_tensor)
+            expected_shape = tf.shape(answer_tensor)
+            output_shape = tf.shape(output)
+            pad_size = tf.maximum(expected_shape[1] - output_shape[1], 0)
+            paddings = [[0, 0], [0, pad_size], [0, 0]]
+            output = tf.pad(output, paddings, constant_values=0.0)
+            output = output[:, :expected_shape[1], :]
+            loss = tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(answer_tensor, output, from_logits=True))
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        return loss
+
     num_epochs = 100
     for epoch in range(num_epochs):
         total_loss = 0
         for i in range(dataset_size):
             question_tensor = text_to_tensor(questions[i], char_to_idx, max_length)
             answer_tensor = text_to_tensor(answers[i], char_to_idx, max_length)
-
-            def train_step(inputs):
-                question_tensor, answer_tensor = inputs
-                with tf.GradientTape() as tape:
-                    output = model(question_tensor)
-                    # Ensure the output shape matches the target shape
-                    expected_shape = tf.shape(answer_tensor)
-                    output_shape = tf.shape(output)
-                    pad_size = tf.maximum(expected_shape[1] - output_shape[1], 0)
-                    paddings = [[0, 0], [0, pad_size], [0, 0]]
-                    output = tf.pad(output, paddings, constant_values=0.0)
-                    output = output[:, :expected_shape[1], :]  # Trim output to match sequence length
-                    loss = tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(answer_tensor, output, from_logits=True))
-                grads = tape.gradient(loss, model.trainable_variables)
-                optimizer.apply_gradients(zip(grads, model.trainable_variables))
-                return loss
-
-
-
-            per_replica_losses = strategy.run(train_step, args=((tf.constant([question_tensor], dtype=tf.int32), tf.constant([answer_tensor], dtype=tf.int32)),))
-            total_loss += strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
+            loss = strategy.run(train_step, args=(tf.constant([question_tensor], dtype=tf.int32), tf.constant([answer_tensor], dtype=tf.int32)))
+            total_loss += loss
 
         print('Epoch [{}/{}], Loss: {:.5f}'.format(epoch+1, num_epochs, total_loss/dataset_size))
 
 if __name__ == "__main__":
-    # 设置分布式参数
     ip_list = ["208.68.39.112:12345", "143.244.164.42:12345", "208.68.36.142:12345", "178.128.148.143:12345", "157.230.88.11:12345"]
-    num_workers = len(ip_list)
-    # 设置分布式参数
     cluster_resolver = tf.distribute.cluster_resolver.TFConfigClusterResolver(ip_list)
-
-    # 指定本地IP地址
     cluster_resolver.task_type = 'worker'
+    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy(cluster_resolver)
 
-    # 指定任务ID
-    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
-
-    # 开始训练
     train(strategy, questions, answers, char_to_idx, max_length)
