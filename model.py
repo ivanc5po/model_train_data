d6cc9bf83ed54fe99ba26c2720b455b8575fa52c
ivanpoopoo <59787536+ivanc5po@users.noreply.github.com>
2024-03-24 01:47:44 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,6 @@
-import os
+import tensorflow as tf
+import numpy as np
 import socket
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.distributed as dist
-import torch.multiprocessing as mp
 
 # 数据集，假设有一组问题和对应的回答
 questions = open("questions.txt", "r", encoding="utf-8").readlines()
@@ -19,43 +15,42 @@ idx_to_char = {i: ch for i, ch in enumerate(chars)}
 def text_to_tensor(text, char_to_idx, max_length):
     tensor = [char_to_idx[ch] for ch in text if ch in char_to_idx]
     tensor += [0] * (max_length - len(tensor)) 
-    return torch.tensor(tensor, dtype=torch.long).unsqueeze(0)
+    return np.array(tensor)
 
 # 获取最大长度
 max_length = max(max(len(question), len(answer)) for question, answer in zip(questions, answers))
 
 # 构建问答模型
-class QALSTM(nn.Module):
+class QALSTM(tf.keras.Model):
     def __init__(self, input_size, hidden_size, num_layers, output_size, num_heads):
         super(QALSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.multihead_attn = nn.MultiheadAttention(hidden_size, num_heads)
-        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.embedding = tf.keras.layers.Embedding(input_size, hidden_size)
+        self.multihead_attn = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=hidden_size)
+        self.lstm = tf.keras.layers.LSTM(hidden_size, return_sequences=True)
+        self.fc = tf.keras.layers.Dense(output_size)
 
-    def forward(self, x):
+    def call(self, x):
         x = self.embedding(x)
-        x = x.permute(1, 0, 2)  # 将 batch 维度放在第二维
-        x, _ = self.multihead_attn(x, x, x)  # 多头自注意力
-        x, _ = self.lstm(x)
-        x = self.fc(x)
-        return x
+        attn_output = self.multihead_attn(x, x, x)
+        lstm_output = self.lstm(attn_output)
+        output = self.fc(lstm_output)
+        return output
 
 def train(rank, world_size, device_ips, port):
     # 获取本机 IP 地址
     local_ip = socket.gethostbyname(socket.gethostname())
-    os.environ['MASTER_ADDR'] = local_ip
-    os.environ['MASTER_PORT'] = port
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
-    # 等待所有节点上线
-    if rank == 0:
-        print("Waiting for all nodes to come online...")
-    dist.barrier()
+    # 设置分布式参数
+    cluster_spec = tf.train.ClusterSpec({"worker": device_ips})
+    server = tf.distribute.Server(cluster_spec, job_name="worker", task_index=rank)
 
-    print("正在載入模型, 節點:{rank}......")
+    # 连接到集群
+    tf.config.experimental_connect_to_cluster(server.target)
+    tf.distribute.experimental_set_strategy(tf.distribute.experimental.MultiWorkerMirroredStrategy())
+
+    print("正在載入模型, 節點:", rank)
     
     # 模型参数
     input_size = len(chars)  # 输入大小为字符集大小
@@ -65,42 +60,35 @@ def train(rank, world_size, device_ips, port):
     num_heads = 8  # 多头注意力的头数
 
     # 创建模型和优化器
-    device = torch.device("cpu")
-    torch.manual_seed(0)  # 为了保证可重复性，设置随机种子
-    model = QALSTM(input_size, hidden_size, num_layers, output_size, num_heads).to(device)
-    model = nn.parallel.DistributedDataParallel(model)
-    criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-    # 同步所有进程
-    dist.barrier()
+    with tf.device("/job:worker/task:{}".format(rank)):
+        model = QALSTM(input_size, hidden_size, num_layers, output_size, num_heads)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
     # 数据集大小
     dataset_size = len(questions)
 
     # 训练模型
     num_epochs = 100
-    print(f"開始訓練, 節點:{rank}......")
+    print("開始訓練, 節點:", rank)
     for epoch in range(num_epochs):
-        model.train()
         total_loss = 0
         for i in range(rank, dataset_size, world_size):
-            question_tensor = text_to_tensor(questions[i], char_to_idx, max_length).to(device)
-            answer_tensor = text_to_tensor(answers[i], char_to_idx, max_length).to(device)
+            question_tensor = text_to_tensor(questions[i], char_to_idx, max_length)
+            answer_tensor = text_to_tensor(answers[i], char_to_idx, max_length)
 
-            optimizer.zero_grad()
-            outputs = model(question_tensor)
+            with tf.GradientTape() as tape:
+                output = model(tf.constant([question_tensor], dtype=tf.int32))
+                loss = tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(answer_tensor, output, from_logits=True))
 
-            loss = criterion(outputs.view(-1, output_size), answer_tensor.view(-1))
-            total_loss += loss.item()
+            gradients = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-            loss.backward()
-            optimizer.step()
-            print(f'Device {rank} - Epoch [{epoch+1}/{num_epochs}], data [{i}/{dataset_size}], Loss: {total_loss/(i+1):.5f}')
+            total_loss += loss.numpy()
+            print('Device {} - Epoch [{}/{}], data [{}/{}], Loss: {:.5f}'.format(rank, epoch+1, num_epochs, i, dataset_size, total_loss/(i+1)))
 
-        
 if __name__ == "__main__":
-    device_ips = "208.68.39.112 143.244.164.42 208.68.36.142 178.128.148.143 157.230.88.11".split()
+    device_ips = ["208.68.39.112", "143.244.164.42", "208.68.36.142", "178.128.148.143", "157.230.88.11"]
     port = "12345"  # 设置端口号
     world_size = len(device_ips)  # 设置世界大小，即使用的设备数量
-    mp.spawn(train, args=(world_size, device_ips, port), nprocs=world_size, join=True)
+    for i in range(world_size):
+        train(i, world_size, device_ips, port)
